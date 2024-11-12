8f9274413d6568714978c32589cefa629f6a4832
MichaelD1B <102457@glr.nl>
2024-11-12 09:51:07 +0000

    Main

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');  // Kleine script toegevoegd omdat ik de website op mobiel wou laten werken
+    navLinks.classList.toggle('open');
+}
